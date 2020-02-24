8f9ae0cbc3da7a2bffa9957037adb8beb0f78bfa
ivanetra <ivanekaputra24@gmail.com>
2020-02-24 15:27:23 +0700

    Call data in TasksData.java file

--- app/src/main/java/com/example/taskman/MainActivity.java
+++ app/src/main/java/com/example/taskman/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.taskman;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvTasks;
+    private ArrayList<Task> list = new ArrayList<>();
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
         rvTasks = findViewById(R.id.rv_tasks);
         rvTasks.setHasFixedSize(true);
+
+        list.addAll(TasksData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvTasks.setLayoutManager(new LinearLayoutManager(this));
+        ListTaskAdapter listTaskAdapter = new ListTaskAdapter(list);
+        rvTasks.setAdapter(listTaskAdapter);
     }
 }
