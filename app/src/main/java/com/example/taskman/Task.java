f2428e98e9662c1d0c619d5e8d6234230b8fea0d
ivanetra <ivanekaputra24@gmail.com>
2020-02-24 14:22:24 +0700

    Create setter getter

--- app/src/main/java/com/example/taskman/Task.java
+++ app/src/main/java/com/example/taskman/Task.java
@@ -1,4 +1,24 @@
 package com.example.taskman;
 
+import java.util.Date;
+
 public class Task {
+    private String name;
+    private Date deadline;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
 }
