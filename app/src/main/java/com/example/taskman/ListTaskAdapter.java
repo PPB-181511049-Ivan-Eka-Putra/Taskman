7717c9556fcbcca20fc583119aaf2e05017959d7
ivanetra <ivanekaputra24@gmail.com>
2020-02-24 14:47:25 +0700

    Create constructor and list

--- app/src/main/java/com/example/taskman/ListTaskAdapter.java
+++ app/src/main/java/com/example/taskman/ListTaskAdapter.java
@@ -1,4 +1,44 @@
 package com.example.taskman;
 
-public class ListTaskAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListTaskAdapter extends RecyclerView.Adapter<ListTaskAdapter.ListViewHolder> {
+    private ArrayList<Task> listTask;
+
+    public ListTaskAdapter(ArrayList<Task> list) {
+        this.listTask = list;
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName, tvTimeRemaining;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tv_item_name);
+            tvTimeRemaining = itemView.findViewById(R.id.tv_item_time_remaining);
+        }
+    }
 }
