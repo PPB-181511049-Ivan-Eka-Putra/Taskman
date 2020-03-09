020dd81a62fe6f8bf30614a008ec73fbc3a3a50a
ivanetra <ivanekaputra24@gmail.com>
2020-03-09 15:53:28 +0700

    Create function for move to create new task activity

--- app/src/main/java/com/example/taskman/MainActivity.java
+++ app/src/main/java/com/example/taskman/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -38,5 +42,10 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, data.getData().toString(), Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    public void createNewTask(View view) {
+        Intent intent = new Intent(this, CreateNewTaskActivity.class);
+        this.startActivity(intent);
     }
 }
