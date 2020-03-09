7e7d06f1f174d16ddebbf7e75f656264b418426c
ivanetra <ivanekaputra24@gmail.com>
2020-03-09 15:52:36 +0700

    Create create new task activity

--- app/src/main/java/com/example/taskman/CreateNewTaskActivity.java
+++ app/src/main/java/com/example/taskman/CreateNewTaskActivity.java
@@ -2,13 +2,42 @@ package com.example.taskman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class CreateNewTaskActivity extends AppCompatActivity {
+
+    EditText etTaskName;
+    EditText etTaskDeadline;
+
+    DBAdapter db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_task);
+
+        etTaskName = findViewById(R.id.et_task_name);
+        etTaskDeadline = findViewById(R.id.et_task_deadline);
+
+        db = new DBAdapter(this);
+    }
+
+    public void insertTask(View view) {
+        // Add a task
+        db.open();
+        long id = db.insertTask(etTaskName.getText().toString(), etTaskDeadline.getText().toString());
+        db.close();
+        finish();
     }
 }
