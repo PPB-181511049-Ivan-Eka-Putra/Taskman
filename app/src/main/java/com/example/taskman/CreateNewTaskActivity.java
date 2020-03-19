c0c35f089b9cdb62ca4953454b67011fb59e0151
ivanetra <ivanekaputra24@gmail.com>
2020-03-19 21:29:43 +0700

    Remove insert task to SQLite method

--- app/src/main/java/com/example/taskman/CreateNewTaskActivity.java
+++ app/src/main/java/com/example/taskman/CreateNewTaskActivity.java
@@ -27,8 +27,6 @@ public class CreateNewTaskActivity extends AppCompatActivity {
     EditText etTaskDeadline;
     Button b;
 
-    DBAdapter db;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,29 +36,18 @@ public class CreateNewTaskActivity extends AppCompatActivity {
         etTaskDeadline = findViewById(R.id.et_task_deadline);
 
         b = findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent replyIntent = new Intent();
-                if (TextUtils.isEmpty(etTaskName.getText()) && TextUtils.isEmpty(etTaskDeadline.getText())) {
-                    setResult(RESULT_CANCELED, replyIntent);
-                } else {
-                    String taskName = etTaskName.getText().toString();
-                    String taskDeadline = etTaskDeadline.getText().toString();
-                    replyIntent.putExtra(EXTRA_TASK_NAME, taskName);
-                    replyIntent.putExtra(EXTRA_TASK_DEADLINE, taskDeadline);
-                    setResult(RESULT_OK, replyIntent);
-                }
-                finish();
+        b.setOnClickListener(view -> {
+            Intent replyIntent = new Intent();
+            if (TextUtils.isEmpty(etTaskName.getText()) && TextUtils.isEmpty(etTaskDeadline.getText())) {
+                setResult(RESULT_CANCELED, replyIntent);
+            } else {
+                String taskName = etTaskName.getText().toString();
+                String taskDeadline = etTaskDeadline.getText().toString();
+                replyIntent.putExtra(EXTRA_TASK_NAME, taskName);
+                replyIntent.putExtra(EXTRA_TASK_DEADLINE, taskDeadline);
+                setResult(RESULT_OK, replyIntent);
             }
+            finish();
         });
-    }
-
-    public void insertTask(View view) {
-        // Add a task
-        db.open();
-        long id = db.insertTask(etTaskName.getText().toString(), etTaskDeadline.getText().toString());
-        db.close();
-        finish();
     }
 }
