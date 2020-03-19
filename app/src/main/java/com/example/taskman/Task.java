8de72d815960f3cfe3b390d9917fbd4714dd6a11
ivanetra <ivanekaputra24@gmail.com>
2020-03-19 15:12:16 +0700

    Update Task class with annotations

--- app/src/main/java/com/example/taskman/Task.java
+++ app/src/main/java/com/example/taskman/Task.java
@@ -3,81 +3,62 @@ package com.example.taskman;
 
 import android.os.Build;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
+@Entity(tableName = "task_table")
 public class Task {
-    private String name;
-    private Date deadline;
-    private String status;
-    private boolean type;
 
-    public String getName() {
-        return name;
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    private  int mId;
+
+    @NonNull
+    @ColumnInfo(name = "name")
+    private String mName;
+
+    @NonNull
+    @ColumnInfo(name = "deadline")
+    private Date mDeadline;
+
+    @NonNull
+    @ColumnInfo(name = "status")
+    private String mStatus;
+
+    @NonNull
+    @ColumnInfo(name = "type")
+    private boolean mType;
+
+    public Task(@NonNull int id, @NonNull String name, @NonNull Date deadine, @NonNull String status, @NonNull boolean type) {
+        this.mId = id;
+        this.mName = name;
+        this.mDeadline = deadine;
+        this.mStatus = status;
+        this.mType = type;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getId() {
+        return mId;
+    }
+
+    public String getName() {
+        return mName;
     }
 
     public Date getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(Date deadline) {
-        this.deadline = deadline;
+        return mDeadline;
     }
 
     public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public boolean isType() {
-        return type;
-    }
-
-    public void setType(boolean type) {
-        this.type = type;
-    }
-
-    public String CalculateTimeRemaining() {
-        String timeRemaining;
-
-        long different = deadline.getTime() - Calendar.getInstance().getTimeInMillis();
-
-        long secondsInMilli = 1000;
-        long minutesInMilli = secondsInMilli * 60;
-        long hoursInMilli = minutesInMilli * 60;
-        long daysInMilli = hoursInMilli * 24;
-
-        long elapsedDays = different / daysInMilli;
-        different = different % daysInMilli;
-
-        long elapsedHours = different / hoursInMilli;
-        different = different % hoursInMilli;
-
-        long elapsedMinutes = different / minutesInMilli;
-        different = different % minutesInMilli;
-
-        long elapsedSeconds = different / secondsInMilli;
-
-        if (elapsedDays != 0) {
-            timeRemaining = elapsedDays + " days " + elapsedHours + " hours " + elapsedMinutes + " minutes " + elapsedSeconds + " seconds";
-        } else if (elapsedHours != 0) {
-            timeRemaining = elapsedHours + " hours " + elapsedMinutes + " minutes " + elapsedSeconds + " seconds";
-        } else if (elapsedMinutes != 0) {
-            timeRemaining = elapsedMinutes + " minutes " + elapsedSeconds + " seconds";
-        } else {
-            timeRemaining = elapsedSeconds + " seconds";
-        }
-        return timeRemaining;
+        return mStatus;
     }
 }
