6ea1cbff3606884226f796d494426a29fa560b06
ivanetra <ivanekaputra24@gmail.com>
2020-03-19 15:15:48 +0700

    Fix the imports

--- app/src/main/java/com/example/taskman/TaskDao.java
+++ app/src/main/java/com/example/taskman/TaskDao.java
@@ -1,4 +1,14 @@
 package com.example.taskman;
 
-public class TaskDao {
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+
+@Dao
+public interface TaskDao {
+
+    // allowing the insert of the same word multiple times by passing a
+    // conflict resolution strategy
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Task word);
 }
