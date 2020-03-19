06f5758e95274cb22e8e9e6a01654ced6736262e
ivanetra <ivanekaputra24@gmail.com>
2020-03-19 17:26:23 +0700

    Add the callback to the database build sequence right before calling .build() on the Room.databaseBuilder()

--- app/src/main/java/com/example/taskman/TaskRoomDatabase.java
+++ app/src/main/java/com/example/taskman/TaskRoomDatabase.java
@@ -1,4 +1,43 @@
 package com.example.taskman;
 
-public class TaskRoomDatabase {
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Task.class}, version = 1, exportSchema = false)
+@TypeConverters({Converters.class})
+public abstract class TaskRoomDatabase extends RoomDatabase {
+
+    public abstract TaskDao taskDao();
+
+    private static volatile TaskRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriterExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static TaskRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TaskRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), TaskRoomDatabase.class, "task_database").addCallback(sRoomDatabaseCallback).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+        }
+    };
 }
