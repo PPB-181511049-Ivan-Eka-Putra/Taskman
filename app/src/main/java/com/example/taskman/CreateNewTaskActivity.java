d58010ccb3da8f3e9adc8673a1be9be6698a1673
ivanetra <ivanekaputra24@gmail.com>
2020-03-19 17:36:56 +0700

    Update the code for the Create New Task Activity

--- app/src/main/java/com/example/taskman/CreateNewTaskActivity.java
+++ app/src/main/java/com/example/taskman/CreateNewTaskActivity.java
@@ -6,10 +6,13 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -17,8 +20,12 @@ import java.util.Locale;
 
 public class CreateNewTaskActivity extends AppCompatActivity {
 
+    public static final String EXTRA_TASK_NAME = "com.example.android.tasklistsql.TASK_NAME";
+    public static final String EXTRA_TASK_DEADLINE = "com.example.android.tasklistsql.TASK_DEADLINE";
+
     EditText etTaskName;
     EditText etTaskDeadline;
+    Button b;
 
     DBAdapter db;
 
@@ -30,7 +37,23 @@ public class CreateNewTaskActivity extends AppCompatActivity {
         etTaskName = findViewById(R.id.et_task_name);
         etTaskDeadline = findViewById(R.id.et_task_deadline);
 
-        db = new DBAdapter(this);
+        b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent replyIntent = new Intent();
+                if (TextUtils.isEmpty(etTaskName.getText()) && TextUtils.isEmpty(etTaskDeadline.getText())) {
+                    setResult(RESULT_CANCELED, replyIntent);
+                } else {
+                    String taskName = etTaskName.getText().toString();
+                    String taskDeadline = etTaskDeadline.getText().toString();
+                    replyIntent.putExtra(EXTRA_TASK_NAME, taskName);
+                    replyIntent.putExtra(EXTRA_TASK_DEADLINE, taskDeadline);
+                    setResult(RESULT_OK, replyIntent);
+                }
+                finish();
+            }
+        });
     }
 
     public void insertTask(View view) {
